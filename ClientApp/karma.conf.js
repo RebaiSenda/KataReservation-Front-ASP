ad442675a19de8fe48ef3d3e06b5228792ded60e
RebaiSenda <rebaisenda93@gmail.com>
2025-04-14 15:00:03 +0200

    Update karma.js

--- ClientApp/karma.conf.js
+++ ClientApp/karma.conf.js
@@ -25,8 +25,34 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
+
+    // Utilisez Chrome normal pour le débogage
+    browsers: ['ChromeCustom'],
+    customLaunchers: {
+      ChromeCustom: {
+        base: 'Chrome',
+        flags: [
+          '--no-sandbox',
+          '--disable-gpu',
+          '--disable-extensions',
+          '--remote-debugging-port=9222',
+          '--enable-logging' // Ajouté pour le logging
+        ]
+      }
+    },
+
+    // Paramètres de tolérance pour éviter les déconnexions
+    captureTimeout: 300000, // Augmenté pour plus de stabilité
+    browserDisconnectTolerance: 3,
+    browserDisconnectTimeout: 15000,
+    browserNoActivityTimeout: 300000,
+    processKillTimeout: 5000,
+
+    // Options supplémentaires pour la stabilité
+    failOnEmptyTestSuite: false,
+    retryLimit: 0,
+
+    singleRun: false,  // Changer à 'true' si vous exécutez dans un CI ou si vous voulez juste une exécution unique
     restartOnFileChange: true
   });
 };
